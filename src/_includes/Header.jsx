54bb901c8ed59448fe3b60822c54c8f17cc4ab7b
wommy <tom@technomad.media>
2022-02-08 13:05:57 -0500

    saving

---
+++ src/_includes/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button';
+
+const Header = ({ title }) => {
+	const onClick = () => {
+		console.log('Click')
+	}
+	return (
+		<header className='header'>
+				<h1>{title}</h1>
+				<Button color='green' text='Add' onClick={onClick} />
+			</header>
+	);
+};
+
+export default Header;
---
+++ src/trav/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Header from '../_includes/Header'
+import Tasks from '../_includes/Tasks'
+
+export const frontMatter = {
+  layout: 'layout',
+  hydrate: 'eager',
+  title: 'Task Tracker',
+}
+
+function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: "1",
+			text: "Doctors Appointment",
+			day: "Feb 5th at 2:30pm",
+			reminder: true,
+		},
+		{
+			id: "2",
+			text: "Meeting at School",
+			day: "Feb 6th at 1:30pm",
+			reminder: true,
+		},
+		{
+			id: "3",
+			text: "Food shopping",
+			day: "Feb 7th at 2:00pm",
+			reminder: false,
+		},
+	])
+
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter( task => task.id !== id ))
+  }
+
+  return (
+    <div className='container'>
+      <Header title='Task Tracker' />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ('No Tasks to Show')}
+    </div>
+  )
+}
+
+export default App
