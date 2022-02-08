198736a6d3cb4adb9192d4015fe41ec279d01d12
wommy <tom@technomad.media>
2022-02-08 15:03:15 -0500

    layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   /**
    * Why copy the /public directory?
-   * 
+   *
    * Slinkity uses Vite (https://vitejs.dev) under the hood for processing styles and JS resources
    * This tool encourages a /public directory for your static assets like social images
    * To ensure this directory is discoverable by Vite, we copy it to our 11ty build output like so:
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
   return {
     /**
      * Why use Nunjucks?
-     * 
+     *
      * We recommend using Nunjucks over Liquid for nicer component shortcode syntax in your markdown
      * See our docs on passing props to components here: https://slinkity.dev/docs/component-shortcodes/#passing-props-to-shortcodes
      * Prefer liquid, or don't mind liquid's shortcode syntax? No problem!
@@ -21,13 +21,14 @@ module.exports = function (eleventyConfig) {
     dir: {
       /**
        * Why set an input directory?
-       * 
+       *
        * By default, 11ty will treat the base of your project as the input.
        * This can have some nasty consequences, like accidentally copying your README.md as a route!
        * You can manually ignore certain files from the build output. But to keep things simple,
        * We recommend setting an input directory like so:
        */
       input: 'src',
+      layouts: '_layouts',
     },
   }
 }
